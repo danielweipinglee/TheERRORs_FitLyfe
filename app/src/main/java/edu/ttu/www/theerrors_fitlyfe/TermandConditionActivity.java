6b7df1b6ef26ebb7d0a35ffb510dc83b6a576f3e
Orion Koepke <orion.koepke@ttu.edu>
2018-02-19 15:31:33 -0600

    Added imports to TermsandConditions.class.

--- app/src/main/java/edu/ttu/www/theerrors_fitlyfe/TermandConditionActivity.java
+++ app/src/main/java/edu/ttu/www/theerrors_fitlyfe/TermandConditionActivity.java
@@ -1,10 +1,14 @@
 package edu.ttu.www.theerrors_fitlyfe;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by Daniel on 2/15/18.
  */
 
-public class TermandConditionActivity {
+public class TermandConditionActivity extends AppCompatActivity {
 
     public void goToMain(View v) {
         Intent intent = new Intent(getBaseContext(), MainActivity.class);

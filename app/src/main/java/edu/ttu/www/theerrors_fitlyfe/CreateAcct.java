ec8b275e05b8841dc828f8e8d6da395af41b0f68
willgray05 <33471153+willgray05@users.noreply.github.com>
2018-02-19 15:44:53 -0600

    Add files via upload

---
+++ app/src/main/java/edu/ttu/www/theerrors_fitlyfe/CreateAcct.java
@@ -0,0 +1,13 @@
+package edu.ttu.www.theerrors_fitlyfe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAcct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acct);
+    }
+}

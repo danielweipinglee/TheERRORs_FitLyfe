ca89d04d6fd98514f13a4abda4a6569c98c34200
JuJoCoe <jordan.j.coe@gmail.com>
2018-03-08 19:32:37 -0600

    Calorie Consumption
    Created Calorie Consumption page off of the Main Page

--- app/src/main/java/edu/ttu/www/theerrors_fitlyfe/MainActivity.java
+++ app/src/main/java/edu/ttu/www/theerrors_fitlyfe/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         advance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, TermandConditionActivity.class);
+                Intent intent = new Intent(MainActivity.this, Calorie_Consumption.class);
                 startActivity(intent);
 
             }
---
+++ app/src/main/java/edu/ttu/www/theerrors_fitlyfe/calorie_consumption.java
@@ -0,0 +1,79 @@
+package edu.ttu.www.theerrors_fitlyfe;
+
+import android.animation.ObjectAnimator;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RotateDrawable;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Calorie_Consumption extends AppCompatActivity {
+
+
+    int pStatus = 0;
+    private Handler handler = new Handler();
+    TextView tv;
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calorie__consumption);
+
+
+        Resources res = getResources();
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(),R.drawable.custom_progressbar_drawable, null);
+        final ProgressBar mProgress = (ProgressBar) findViewById(R.id.circularProgressbar);
+        mProgress.setProgress(0);   // Main Progress
+        mProgress.setSecondaryProgress(100); // Secondary Progress
+        mProgress.setMax(100); // Maximum Progress
+        mProgress.setProgressDrawable(drawable);
+
+      /*  ObjectAnimator animation = ObjectAnimator.ofInt(mProgress, "progress", 0, 100);
+        animation.setDuration(50000);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();*/
+
+        tv = (TextView) findViewById(R.id.tv);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (pStatus < 55) {
+                    pStatus += 1;
+
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            mProgress.setProgress(pStatus);
+                            tv.setText(pStatus + "%");
+
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        // Just to display the progress slowly
+                        Thread.sleep(16); //thread will take approx 1.5 seconds to finish
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}

748089e1ab6d558589272d196481024bb8ca5eea
ryanfaulkenberry100 <ryan.faulkenberry@ttu.edu>
2018-04-11 13:23:45 -0500

    Main Activity tests pass

--- app/src/androidTest/java/edu/ttu/www/theerrors_fitlyfe/MainActivityTest.java
+++ app/src/androidTest/java/edu/ttu/www/theerrors_fitlyfe/MainActivityTest.java
@@ -3,10 +3,15 @@ package edu.ttu.www.theerrors_fitlyfe;
 import android.app.Activity;
 import android.app.Instrumentation;
 import android.content.Intent;
+import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.intent.rule.*;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitor;
+import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import android.view.View;
 import android.widget.Button;
 
 import org.junit.Before;
@@ -18,8 +23,10 @@ import org.hamcrest.*;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
@@ -39,7 +46,28 @@ public class MainActivityTest {
     @Rule
     public IntentsTestRule<Calorie_Consumption> intentsTestRule =
             new IntentsTestRule<>(Calorie_Consumption.class);
+/*
+    // Login before running each test
+    @Before
+    public void logMeIn() {
+        // We might need to login and accept T&C
+        if (getCurrentActivity().getLocalClassName() == "LoginActivity") {
+            String dummyEmail = new String("testyboy@gmail.com");
+            String dummyPassword = new String("password");
 
+            // Sign in with test account
+            onView(withId(R.id.email)).perform(typeText(dummyEmail));
+            onView(withId(R.id.password)).perform(typeText(dummyPassword));
+            onView(withId(R.id.sign_in_button)).perform(click());
+
+            if (getCurrentActivity().getLocalClassName() == "TermandConditionActivity") {
+                // Accept EULA
+                onView(withId(R.id.acceptCheckBox)).perform(click());
+                onView(withId(R.id.continueButton)).perform(click());
+            }
+        }
+    }
+*/
     /*
      * Ensure each button launches the correct intent
      */
@@ -68,7 +96,7 @@ public class MainActivityTest {
     @Test
     public void exerciseLogIntentTest() {
         // Click exercise log button
-        onView(withId(R.id.calorie_consumption_button))
+        onView(withId(R.id.exercise_log_button))
                 .perform(click());
 
         // Make sure the exercise log page is launched
@@ -79,7 +107,7 @@ public class MainActivityTest {
     @Test
     public void sleepTrackingIntentTest() {
         // Click sleep tracking button
-        onView(withId(R.id.calorie_consumption_button))
+        onView(withId(R.id.sleep_tracking_button))
                 .perform(click());
 
         // Make sure the sleep tracking page is launched

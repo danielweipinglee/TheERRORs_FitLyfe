645ff4e0b2e7c49042fd8c8c1e768ad5893ff9ca
Orion Koepke <orion.koepke@ttu.edu>
2018-04-21 13:58:40 -0500

    I added code to add in the goals that the user puts in.

--- app/src/main/java/edu/ttu/www/theerrors_fitlyfe/GoalSetting.java
+++ app/src/main/java/edu/ttu/www/theerrors_fitlyfe/GoalSetting.java
@@ -3,6 +3,7 @@ package edu.ttu.www.theerrors_fitlyfe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,15 +16,24 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class GoalSetting extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
     /*
     Use variable "goaltype" when deciding what kind of goal to store the value as
      */
     private FirebaseAuth mAuth;
+    public FirebaseUser curUser;
+
     String goaltype;
+
+    public Spinner spinner;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_setting);
@@ -33,7 +43,7 @@ public class GoalSetting extends AppCompatActivity implements AdapterView.OnItem
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //initializes the spinner
-        Spinner spinner = findViewById(R.id.spinner);
+        spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.goal_type,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -48,7 +58,21 @@ public class GoalSetting extends AppCompatActivity implements AdapterView.OnItem
         });
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
 
+        mAuth = FirebaseAuth.getInstance();
+
+        // Get the current user that is logged in, if there is one.
+        curUser = mAuth.getCurrentUser();
+
+        // If there is no current user logged in, then send them to the login page.
+        if(curUser == null) {
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+    }
 
     /*
     When an item is selected it will grab the text and store it in a variable so we will know
@@ -77,6 +101,51 @@ public class GoalSetting extends AppCompatActivity implements AdapterView.OnItem
         float GoalInt = Float.parseFloat(GoalS);
         goal.setText("");
 
+        String goalType = spinner.getSelectedItem().toString();
+
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        final DatabaseReference goals = database.child(curUser.getUid()).child("Goals");
+
+        switch(goalType){
+            case "Blood Sugar":
+                goals.child("Blood Sugar").child("Goal").setValue(GoalInt);
+                break;
+            case "Blood Pressure":
+                goals.child("Blood Pressure").child("Goal").setValue(GoalInt);
+                break;
+            case "Calories":
+                goals.child("Calories").child("Goal").setValue(GoalInt);
+                break;
+            case "Exercise":
+                goals.child("Exercise").child("Goal").setValue(GoalInt);
+                break;
+            case "Sleep":
+                goals.child("Sleep").child("Goal").setValue(GoalInt);
+                break;
+            case "Water":
+                goals.child("Water").child("Goal").setValue(GoalInt);
+                break;
+            case "Weight":
+                goals.child("Weight").child("Goal").setValue(GoalInt);
+
+                database.child(curUser.getUid()).child("weight").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        Long weight = (Long) dataSnapshot.getValue();
+                        if(weight != null){
+                            goals.child("Weight").child("Start").setValue(weight);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                break;
+        }
+
         finish();
     }
 
@@ -86,6 +155,7 @@ public class GoalSetting extends AppCompatActivity implements AdapterView.OnItem
         getMenuInflater().inflate(R.menu.inputmenu, menu);
         return true;
     }
+
     //Method functionality for action button
     public boolean onOptionsItemSelected(MenuItem item){
         //Gets the id of the button that was pressed
